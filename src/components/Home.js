1b23c59b37bdfcd7c3f4fc64e67b6e7d5fb8a1f3
Vania <Van.carvajal@duocuc.cl>
2025-10-14 19:54:22 -0300

    Creando la página Blog

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+        <h1>Bienvenido a la página principal</h1>
+        <p>Este es el contenido de la página Home.</p>
+        </div>
+    );
+}
+
+export default Home;
