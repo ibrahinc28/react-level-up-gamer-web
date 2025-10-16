5ebe425e7a05a5e910016f452521cb494f35fd87
Vania <Van.carvajal@duocuc.cl>
2025-10-15 22:07:25 -0300

    Cambios en app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import MyNavbar from './components/MyNavbar';
 import ContactoPage from './pages/ContactoPage'; 
 
-import './App.css'; 
+import './App.css';
 
 function App() {
     return (
         <div className="App">
-            
-            {/* 1. La barra de navegación debe ir en la parte superior */}
             <MyNavbar />
-            
-            {/* 2. El contenido principal de la página */}
             <main>
-                {/* Actualmente solo muestra la página de Contacto */}
                 <ContactoPage />
             </main>
             
