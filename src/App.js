8dc64bce5ffb68c36461405dd6bda7bee3a03237
Vania <Van.carvajal@duocuc.cl>
2025-10-19 23:22:13 -0300

    css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 import MyNavbar from './components/MyNavbar'; 
 import BlogPage from './pages/BlogPage'; 
 
